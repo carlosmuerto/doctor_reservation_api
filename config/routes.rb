7a4732f00537fbac8440ed318bac643eb5e97acf
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-05 21:19:04 +0000

    generate model user for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'users/current'
+  devise_for :users
+  get 'users/current'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
