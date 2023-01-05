27e6a6a7078bfb2ac2d53e4a2ee01d00f0d79381
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-05 21:21:32 +0000

    generate devise need controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  
   get 'users/current'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
