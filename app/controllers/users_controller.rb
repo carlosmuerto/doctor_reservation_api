83e824f41116cbc3a01d80b83310f470afcc361a
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-05 22:46:02 +0000

    Configure devise controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def current
-    render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    # render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    render json: current_user, status: :ok
   end
 end
