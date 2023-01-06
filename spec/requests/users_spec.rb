78777b446400670bdf918f3d636f6ec8640d2e12
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-06 00:46:05 +0000

    Linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /current" do
-    it "returns http success" do
-      get "/users/current"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /current' do
+    it 'returns http success' do
+      get '/users/current'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
