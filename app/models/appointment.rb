687e27d5ec2c76127e7a57be4e6ffc398eae120b
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-10 00:05:49 +0000

    Update ruby version and fix minor problems

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,10 +3,12 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
 
   # validations
-  # validate :date_of_appointment_cannot_be_in_the_past
+  # validate :datetime_of_appointment_cannot_be_in_the_past
   validates :description, presence: true, length: { in: 3..150 }
-  validates :date_of_appointment, presence: true,
-                                  inclusion: { in: Date.today..Date.today + 5.years, message: 'Date should be in the future' }
-  validates :time_of_appointment, presence: true,
-                                  format: { with: /\A\d{2}:\d{2}\z/, message: 'Time format should be HH:MM' }
+  validates :datetime_of_appointment,
+            presence: true,
+            inclusion: {
+              in: Date.today..Date.today + 5.years,
+              message: 'Date should be in the future and no more that 5 years'
+            }
 end
