836ab49efccbacd8a034bd50e406ef396277ca84
Matias Aguirre <aguirremati09@gmail.com>
2023-01-11 17:54:36 -0300

    Routes Created

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#current"
+
+  resources :users, only: [:index, :create, :login] do
+  resources :appointments, only: [:index, :create, :show, :update, :destroy]
+  resources :doctors, only: [:create, :destroy]
+
+  end
+
+  post 'users/login', to: 'users#login'
+  post '/doctors/:id/appointments', to: 'appointments#create_on_doctors'
+  get '/users/:id/doctors', to: 'doctors#show_user_doctors'   
+  get '/appointments', to: 'appointments#show_all'
+  get '/appointments/:id', to: 'appointments#show'
+
+  get '/doctors', to: 'doctors#index'   
+  get '/doctors/:id', to: 'doctors#show'
+  put '/doctors/:id', to: 'doctors#update'
+
+  
+ end
+
 end
