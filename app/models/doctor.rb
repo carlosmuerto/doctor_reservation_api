b488365c3902585b958756be781a32204841823d
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-19 22:34:27 +0000

    add change image

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,9 +1,9 @@
 class Doctor < ApplicationRecord
   belongs_to :user
   has_many :appointments
+  has_one_attached :image
 
   # validations
   validates :name, presence: true, length: { in: 3..50 }
   validates :specialization, presence: true, length: { in: 3..50 }
-  validates :photo, presence: true
 end
